69cfd9d8339cf0c86d6b35aca00dcefd518020f7
Yago Souza <yagos-s@live.com>
2023-04-20 21:32:12 -0300

    inicio do projeto, criação de banco de dados e primeira requisição usando post

---
+++ novo-video.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbPath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$dbPath");
+
+$sql = 'INSERT INTO videos (url, title) VALUES (?, ?)';
+$statement = $pdo->prepare($sql);
+$statement->bindValue(1, $_POST['url']);
+var_dump($_POST['url']);
+$statement->bindValue(2, $_POST['titulo']);
+
+var_dump($statement->execute());
