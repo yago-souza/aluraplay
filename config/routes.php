d2b85ebb2cdc106347bf8c9ca1835911dc67c860
Yago Souza <yagos-s@live.com>
2023-05-02 20:55:29 -0300

    Implementando arquivo de rotas no index

---
+++ config/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'GET|/' => \Yago\Aluraplay\Controller\VideoListController::class,
+    'GET|/novo-video' => \Yago\Aluraplay\Controller\VideoFormController::class,
+    'POST|/novo-video' => \Yago\Aluraplay\Controller\NewVideoController::class,
+    'GET|/editar-video' => \Yago\Aluraplay\Controller\VideoFormController::class,
+    'POST|/editar-video' => \Yago\Aluraplay\Controller\EditVideoController::class,
+    'GET|/remover-video' => \Yago\Aluraplay\Controller\DeleteVideoController::class,
+];
