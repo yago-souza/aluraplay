26a8c93c6067966f7b4e8ff7567f7738ca9859c9
Yago Souza <yagos-s@live.com>
2023-05-02 19:15:01 -0300

    Implementando views

--- src/Controller/VideoListController.php
+++ src/Controller/VideoListController.php
@@ -14,26 +14,6 @@ class VideoListController implements Controller
     public function processaRequisicao(): void
     {
         $videoList = $this->videoRepository->allVideos();
-        require_once __DIR__ . '/../../inicio-html.php';
-        ?>
-        <ul class="videos__container">
-            <?php foreach ($videoList as $video): ?>
-            <li class="videos__item">
-                <iframe width="100%" height="72%" src="<?= $video->getUrl(); ?>"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
-                <div class="descricao-video">
-                    <img src="./img/logo.png" alt="logo canal alura">
-                    <h3><?= $video->getTitulo(); ?></h3>
-                    <div class="acoes-video">
-                        <a href="./editar-video?id=<?=$video->getId(); ?>">Editar</a>
-                        <a href="./remover-video?id=<?=$video->getId(); ?>">Excluir</a>
-                    </div>
-                </div>
-            </li>
-            <?php endforeach; ?>
-        </ul>
-    <?php require_once __DIR__ . '/../../fim-html.php';
+        require_once __DIR__ . '/../../views/video-list.php';
     }
 }
---
+++ views/video-list.php
@@ -0,0 +1,23 @@
+<?php
+/** @var \Yago\Aluraplay\Domain\Model\Video[] $videoList */
+require_once __DIR__ . '/inicio-html.php';
+?>
+    <ul class="videos__container">
+        <?php foreach ($videoList as $video): ?>
+            <li class="videos__item">
+                <iframe width="100%" height="72%" src="<?= $video->getUrl(); ?>"
+                        title="YouTube video player" frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen></iframe>
+                <div class="descricao-video">
+                    <img src="./img/logo.png" alt="logo canal alura">
+                    <h3><?= $video->getTitulo(); ?></h3>
+                    <div class="acoes-video">
+                        <a href="./editar-video?id=<?=$video->getId(); ?>">Editar</a>
+                        <a href="./remover-video?id=<?=$video->getId(); ?>">Excluir</a>
+                    </div>
+                </div>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+<?php require_once __DIR__ . '/fim-html.php';
