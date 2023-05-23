9579fddf45849b24e1ecfadccf9c0038ca69db06
Yago Souza <yagos-s@live.com>
2023-05-23 19:49:18 -0300

    corrigindo ortografia

--- views/video-list.php
+++ views/video-list.php
@@ -20,7 +20,7 @@ require_once __DIR__ . '/inicio-html.php';
                 <h3><?= $video->getTitulo(); ?></h3>
                 <div class="acoes-video">
                     <a href="./editar-video?id=<?=$video->getId(); ?>">Editar</a>
-                    <a href="./remover-thumb?id=<?=$video->getId(); ?>">Reover thumb</a>
+                    <a href="./remover-thumb?id=<?=$video->getId(); ?>">Remover thumb</a>
                     <a href="./remover-video?id=<?=$video->getId(); ?>">Excluir</a>
                 </div>
             </div>
