ca2507648aa7f36b33966f52cbf572d3aed735fd
Yago Souza <yagos-s@live.com>
2023-06-01 21:54:45 -0300

    Revert "Implementando PSR-7 ServerRequestInterface e ResponseInterface (substitui as variaveis globais como $_GET e $_POST por $request->getQueryParams() para get e $request->getParsedBody() para post)"
    This reverts commit 11e40da6

--- teste.php
+++ teste.php
@@ -17,4 +17,3 @@ $videoRepository = new VideoRepository($pdo);
 #var_dump($repositorio->userForEmail("yago@teste.com")->getEmail());
 $video = $videoRepository->allVideos()[2];
 var_dump($video);
-#teste2
